259ae1ecfcdf9b1440838c6e7166aaa817b39923
DeeVonkie <73236199+DeeVonkie@users.noreply.github.com>
2024-12-10 22:03:07 +0100

    Revert "Update"
    This reverts commit cf64b0164a0545f463efc8d7fe05dbf544ecd6fc.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import alpinejs from "@astrojs/alpinejs";
-import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   integrations: [tailwind(), alpinejs()],
-  output: 'server',
-  adapter: vercel(),
 });
