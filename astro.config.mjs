5f200b4513a9943818f2889e280f2e33377b6a90
DeeVonkie <73236199+DeeVonkie@users.noreply.github.com>
2024-12-10 22:28:29 +0100

    Update api routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import alpinejs from "@astrojs/alpinejs";
+import node from "@astrojs/node";
 
 export default defineConfig({
   integrations: [tailwind(), alpinejs()],
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
 });
